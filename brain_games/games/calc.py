704cc7938fed13a0b0eb3ed1e205c9dcd75a66c9
Ваше Имя <you@example.com>
2024-12-15 22:31:08 +0500

    feat(calc): Добавлена игра calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,31 +1,25 @@
-import prompt
 import random
-from brain_games.cli import welcome_user
 
 
 def generate_question():
-    num = random.randint(1, 100)
-    correct_answer = 'yes' if num % 2 == 0 else 'no'
-    return num, correct_answer
+    num1 = random.randint(1, 35)
+    num2 = random.randint(1, 35)
+    
+    operations = ['+', '-', '*']
+    operation = random.choice(operations)
+    
+    match operation:
+        case '+':
+            correct_answer = num1 + num2
+        case '-':
+            correct_answer = num1 - num2
+        case '*':
+            correct_answer = num1 * num2
+    
+    question = f"{num1} {operation} {num2}"
+    
+    return question, str(correct_answer)
 
 
-def game_round():
-    result_message = 'OKAY MAN'
-    return result_message, 1
+hello_message = 'What is the result of the expression?'
 
-
-def run_game():
-    print('Welcome to the Brain Games!')
-    username = welcome_user()
-    print('HELLO WORLD')
-
-    points = 0
-    while points < 3:
-        result_message, result_point = game_round()
-        print(result_message)
-        if result_point == 0:
-            print(f"Let's try again, {username}!")
-            return
-        points += result_point
-
-    print(f'Congratulations, {username}!')
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.calc import run_game
+from brain_games.games.game_runner import run_game
 
 
 def main():
-    run_game()
+    run_game('calc')
 
 
 if __name__ == '__main__':
     main()
-    
